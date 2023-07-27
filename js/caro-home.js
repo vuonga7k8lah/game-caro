aa84f7d807b33d47afdef08770b9499bc37aa477
vuonga7k8lah <vuonga7k8lnc@gmail.com>
2023-07-27 23:10:04 +0700

    update code

---
+++ js/caro-home.js
@@ -0,0 +1,12 @@
+function handleLetGo() {
+    let typePlay = document.getElementById("list-type-play").value;
+    let rows = document.getElementById("list-row").value;
+    let columns = document.getElementById("list-column").value;
+
+    if (typePlay === "" || rows === "" || columns === "") {
+        alert("Vui lòng chọn kiểu chơi");
+        return;
+    }
+    window.location.href =
+        "caro.html?type=" + typePlay + "&rows=" + rows + "&columns=" + columns;
+}
